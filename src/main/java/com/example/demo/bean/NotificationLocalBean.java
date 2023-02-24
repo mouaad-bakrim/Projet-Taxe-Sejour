998b67b2db3be841a50bad935a055cb16e6b203f
Azdoud1 <azdoudmohamed1999@gmail.com>
2023-02-24 16:29:12 +0100

    delete

---
+++ src/main/java/com/example/demo/bean/NotificationLocalBean.java
@@ -0,0 +1,49 @@
+package com.example.demo.bean;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class NotificationLocalBean {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private Local local;
+    private Redevable redevable;
+    private Double mantantEstination;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Local getLocal() {
+        return local;
+    }
+
+    public void setLocal(Local local) {
+        this.local = local;
+    }
+
+    public Redevable getRedevable() {
+        return redevable;
+    }
+
+    public void setRedevable(Redevable redevable) {
+        this.redevable = redevable;
+    }
+
+    public Double getMantantEstination() {
+        return mantantEstination;
+    }
+
+    public void setMantantEstination(Double mantantEstination) {
+        this.mantantEstination = mantantEstination;
+    }
+}
