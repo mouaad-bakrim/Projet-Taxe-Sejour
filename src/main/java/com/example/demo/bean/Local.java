0f3a00a9d2b5aea7601b509364667303a6b8ebf7
NabilAllouche <nabilallouche49@gmail.com>
2023-02-24 16:39:03 +0100

    categorie

--- src/main/java/com/example/demo/bean/Local.java
+++ src/main/java/com/example/demo/bean/Local.java
@@ -8,15 +8,65 @@ public class Local {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
     private String ref;
-    @OneToMany
-    private  Redevable redevable ;
+    int Annee ;
 
-
-
-
+    int derniereAnneePaye ;
+    int dernierTrimistrePaye ;
 
     @ManyToOne
+    private  Redevable redevable ;
+    @ManyToOne
     private  CategorieLocaleSejour categorieLocaleSejour ;
+
+    public String getRef() {
+        return ref;
+    }
+
+    public void setRef(String ref) {
+        this.ref = ref;
+    }
+
+    public int getAnnee() {
+        return Annee;
+    }
+
+    public void setAnnee(int annee) {
+        Annee = annee;
+    }
+
+    public int getDerniereAnneePaye() {
+        return derniereAnneePaye;
+    }
+
+    public void setDerniereAnneePaye(int derniereAnneePaye) {
+        this.derniereAnneePaye = derniereAnneePaye;
+    }
+
+    public int getDernierTrimistrePaye() {
+        return dernierTrimistrePaye;
+    }
+
+    public void setDernierTrimistrePaye(int dernierTrimistrePaye) {
+        this.dernierTrimistrePaye = dernierTrimistrePaye;
+    }
+
+    public Redevable getRedevable() {
+        return redevable;
+    }
+
+    public void setRedevable(Redevable redevable) {
+        this.redevable = redevable;
+    }
+
+    public CategorieLocaleSejour getCategorieLocaleSejour() {
+        return categorieLocaleSejour;
+    }
+
+    public void setCategorieLocaleSejour(CategorieLocaleSejour categorieLocaleSejour) {
+        this.categorieLocaleSejour = categorieLocaleSejour;
+    }
+
+
 
 
 
