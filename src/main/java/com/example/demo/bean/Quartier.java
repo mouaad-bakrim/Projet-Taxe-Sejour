52087009719907fd674ffe10b1e5126a6c8e1579
simosama <babaalimohamed44@gmail.com>
2023-02-26 08:19:01 +0100

    trois beans

---
+++ src/main/java/com/example/demo/bean/Quartier.java
@@ -0,0 +1,46 @@
+package com.example.demo.bean;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Quartier {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private Double code;
+    private String  labelle;
+    @ManyToOne
+    private Secteur secteur;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Double getCode() {
+        return code;
+    }
+
+    public void setCode(Double code) {
+        this.code = code;
+    }
+
+    public String  getLabelle() {
+        return labelle;
+    }
+
+    public void setLabelle(String  labelle) {
+        this.labelle = labelle;
+    }
+
+    public Secteur getSecteur() {
+        return secteur;
+    }
+
+    public void setSecteur(Secteur secteur) {
+        this.secteur = secteur;
+    }
+}
