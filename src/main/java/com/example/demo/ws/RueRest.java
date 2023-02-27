2b1f99c9122823df9a3453a0f2198f33077e5422
Azdoud1 <azdoudmohamed1999@gmail.com>
2023-02-27 19:42:43 +0100

    V

--- src/main/java/com/example/demo/ws/RueRest.java
+++ src/main/java/com/example/demo/ws/RueRest.java
@@ -1,4 +1,30 @@
 package com.example.demo.ws;
 
+import com.example.demo.bean.Rue;
+import com.example.demo.service.RueService;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/rue")
 public class RueRest {
+
+    @Autowired
+    public RueService rueService;
+    @GetMapping("/code/{code}")
+    public Rue findByCode(@PathVariable Double code) {
+        return rueService.findByCode(code);
+    }
+    @DeleteMapping("/code/{code}")
+    @Transactional
+    public int deleteByCode(@PathVariable Double code) {
+        return rueService.deleteByCode(code);
+    }
+    @GetMapping("/code/{code}")
+    public List<Rue> findByQuartierCode(@PathVariable Double Code) {
+        return rueService.findByQuartierCode(Code);
+    }
 }
