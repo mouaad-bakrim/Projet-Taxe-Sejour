161c362764eda07e9767c42b1265f85e9939a368
ismail aouifia <aouifiaismail@gmail.com>
2023-02-27 18:35:08 +0100

    Merge remote-tracking branch 'origin/main'

--- src/main/java/com/example/demo/service/TauxTaxeTrimestrielService.java
+++ src/main/java/com/example/demo/service/TauxTaxeTrimestrielService.java
@@ -1,10 +1,19 @@
 package com.example.demo.service;
 
 
+import com.example.demo.bean.TauxTaxeTrimestriel;
+import com.example.demo.dao.TauxTaxeTrimestrielDao;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class TauxTaxeTrimestrielService {
 
+@Autowired
 
+    private TauxTaxeTrimestrielDao taxeTrimestrielDao;
+
+    public TauxTaxeTrimestriel findByCategorieLocaleSejourCode(String code) {
+        return taxeTrimestrielDao.findByCategorieLocaleSejourCode(code);
+    }
 }
