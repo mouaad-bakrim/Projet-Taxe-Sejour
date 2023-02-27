843c6a89a521f468381387760cc96adb73707817
simosama <babaalimohamed44@gmail.com>
2023-02-27 20:24:52 +0100

    trois beans

--- src/main/java/com/example/demo/dao/QuartierDao.java
+++ src/main/java/com/example/demo/dao/QuartierDao.java
@@ -1,9 +1,7 @@
 package com.example.demo.dao;
 
-<<<<<<< HEAD
 
 import com.example.demo.bean.Quartiere;
-import com.example.demo.bean.Rue;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -12,18 +10,8 @@ import java.util.List;
 @Repository
 public interface QuartierDao extends JpaRepository<Quartiere,Long> {
     List<Quartiere> findAll();
-    public Quartiere findByCode(Double code);
-      int deleteByCode(Double code);
-=======
-import com.example.demo.bean.Quartier;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
-public interface QuartierDao extends JpaRepository<Quartier,Long> {
-    Quartier findByCode(Double code);
+    Quartiere findByCode(Double code);
 
     int deleteByCode(Double code);
->>>>>>> origin/main
-
 }
