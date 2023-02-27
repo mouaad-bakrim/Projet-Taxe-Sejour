2a36b8ebc14275518df38927b61db6f514332c9f
mouaad-bakrim <bakrim2mo@gmail.com>
2023-02-27 19:36:18 +0100

    Rue

--- src/main/java/com/example/demo/service/RueService.java
+++ src/main/java/com/example/demo/service/RueService.java
@@ -1,4 +1,27 @@
 package com.example.demo.service;
 
+import com.example.demo.bean.Rue;
+import com.example.demo.dao.RueDao;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class RueService {
+    @Autowired
+    private RueDao rueDao;
+
+    public Rue findByCode(Double code) {
+        return rueDao.findByCode(code);
+    }
+    @Transactional
+    public int deleteByCode(Double code) {
+        return rueDao.deleteByCode(code);
+    }
+
+    public List<Rue> findByQuartierCode(Double Code) {
+        return rueDao.findByQuartierCode(Code);
+    }
 }
