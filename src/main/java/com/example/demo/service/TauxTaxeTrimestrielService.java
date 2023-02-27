9c72efdf9e846e65e4c5d11113ffd1d5b5a5920a
RedaBenzi <benzireda96@gmail.com>
2023-02-27 19:30:06 +0100

    taux

--- src/main/java/com/example/demo/service/TauxTaxeTrimestrielService.java
+++ src/main/java/com/example/demo/service/TauxTaxeTrimestrielService.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Service;
 @Service
 public class TauxTaxeTrimestrielService {
 
-@Autowired
+    @Autowired
 
     private TauxTaxeTrimestrielDao taxeTrimestrielDao;
 
