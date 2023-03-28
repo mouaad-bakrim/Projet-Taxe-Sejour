66cb286430e4cca0e820745c1921731b2d6284ff
ismail aouifia <aouifiaismail@gmail.com>
2023-03-28 02:22:50 +0000

    Merge remote-tracking branch 'origin/main'

--- src/main/java/com/example/demo/service/impl/TauxTaxeAnuelleServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/TauxTaxeAnuelleServiceImpl.java
@@ -1,6 +1,8 @@
 package com.example.demo.service.impl;
 
 
+import com.example.demo.bean.CategorieLocale;
+import com.example.demo.bean.TauxTaxeAnuelle;
 import com.example.demo.dao.TauxTaxeAnuelleDao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -14,19 +16,29 @@ public class TauxTaxeAnuelleServiceImpl {
     @Autowired
     private  CategorieLocaleServiceImpl categorieLocaleService;
 
+    public int save (TauxTaxeAnuelle tauxTaxeAnuelle){
+            if (tauxTaxeAnuelle.getCategorieLocale() == null){
+                return -1;
+            } else if (tauxTaxeAnuelle.getCategorieLocale() == null) {
+                CategorieLocale categorieLocaleServiceByCode = categorieLocaleService.findOrSave(tauxTaxeAnuelle.getCategorieLocale());
+                tauxTaxeAnuelle.setCategorieLocale(categorieLocaleServiceByCode);
+                tauxTaxeAnuelleDao.save(tauxTaxeAnuelle);
+              return 1  ;
+            }
+           return 2;
+    }
+    public TauxTaxeAnuelle findByCategorieLocaleCodeAndReference(String code, String reference) {
+        return tauxTaxeAnuelleDao.findByCategorieLocaleCodeAndReference(code,reference);
+    }
 
 
+    public TauxTaxeAnuelle findByCategorieLocaleCode(String code) {
+        return tauxTaxeAnuelleDao.findByCategorieLocaleCode(code);
+    }
 
 
-
-
-
-
-
-
-
-
-
-
+    public int deleteByCategorieLocaleCode(String code) {
+        return tauxTaxeAnuelleDao.deleteByCategorieLocaleCode(code);
+    }
 
 }
