b10f002aa27857bdb8eb31260c333c10478034eb
ismail aouifia <aouifiaismail@gmail.com>
2023-03-28 02:26:38 +0000

    Merge remote-tracking branch 'origin/main'

--- src/main/java/com/example/demo/service/impl/TauxTaxeAnuelleServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/TauxTaxeAnuelleServiceImpl.java
@@ -4,11 +4,12 @@ package com.example.demo.service.impl;
 import com.example.demo.bean.CategorieLocale;
 import com.example.demo.bean.TauxTaxeAnuelle;
 import com.example.demo.dao.TauxTaxeAnuelleDao;
+import com.example.demo.service.facade.TauxTaxeAnuelleService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
-public class TauxTaxeAnuelleServiceImpl {
+public class TauxTaxeAnuelleServiceImpl implements TauxTaxeAnuelleService {
 
     @Autowired
     private TauxTaxeAnuelleDao tauxTaxeAnuelleDao;
@@ -27,16 +28,17 @@ public class TauxTaxeAnuelleServiceImpl {
             }
            return 2;
     }
+    @Override
     public TauxTaxeAnuelle findByCategorieLocaleCodeAndReference(String code, String reference) {
         return tauxTaxeAnuelleDao.findByCategorieLocaleCodeAndReference(code,reference);
     }
 
-
+    @Override
     public TauxTaxeAnuelle findByCategorieLocaleCode(String code) {
         return tauxTaxeAnuelleDao.findByCategorieLocaleCode(code);
     }
 
-
+    @Override
     public int deleteByCategorieLocaleCode(String code) {
         return tauxTaxeAnuelleDao.deleteByCategorieLocaleCode(code);
     }
